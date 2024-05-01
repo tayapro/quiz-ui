a6a096ff961a0ccb3107a9d462ae888e4ec47d1b
Taisia Simakova <stipaxa@gmail.com>
2024-05-01 14:21:46 +0100

    Add shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    css: {
+        postcss: {
+            plugins: [tailwind(), autoprefixer()],
+        },
+    },
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 })
