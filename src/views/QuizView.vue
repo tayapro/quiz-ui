afd7264feaca64e378b7b03060f9f1e694c64759
Taisia Simakova <stipaxa@gmail.com>
2024-05-09 21:11:21 +0100

    Update quiz view

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -8,14 +8,33 @@ import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 
 const question = ref('')
 const answers = ref([])
+let answer_index = -1
 
 async function getQuiz() {
     const res = await axios.get(
         'https://96ns4dz4a5.execute-api.eu-west-1.amazonaws.com/dev/api/quiz_question'
     )
-    console.log('Res from quizQuestion', res)
     question.value = res.data.question
     answers.value = res.data.options
+}
+
+async function onSubmit() {
+    if (answer_index === -1) {
+        console.log('Choice answer')
+        return
+    }
+    const res = await axios.post(
+        'https://96ns4dz4a5.execute-api.eu-west-1.amazonaws.com/dev/api/quiz_answer',
+        {
+            question: question.value,
+            answer_index,
+        }
+    )
+    console.log('From handler: ', res)
+}
+
+function onOpt(index) {
+    answer_index = index
 }
 
 onMounted(async () => {
@@ -29,25 +48,41 @@ onMounted(async () => {
             <p class="text-5xl">{{ question }}</p>
         </div>
         <div>
-            <RadioGroup default-value="option-one">
+            <RadioGroup>
                 <div class="flex items-center space-x-2">
-                    <RadioGroupItem id="option-one" value="option-one" />
+                    <RadioGroupItem
+                        id="option-one"
+                        value="option-one"
+                        @click="onOpt(0)"
+                    />
                     <Label for="option-one">{{ answers[0] }}</Label>
                 </div>
                 <div class="flex items-center space-x-2">
-                    <RadioGroupItem id="option-two" value="option-two" />
+                    <RadioGroupItem
+                        id="option-two"
+                        value="option-two"
+                        @click="onOpt(1)"
+                    />
                     <Label for="option-two">{{ answers[1] }}</Label>
                 </div>
                 <div class="flex items-center space-x-2">
-                    <RadioGroupItem id="option-three" value="option-three" />
+                    <RadioGroupItem
+                        id="option-three"
+                        value="option-three"
+                        @click="onOpt(2)"
+                    />
                     <Label for="option-three">{{ answers[2] }}</Label>
                 </div>
                 <div class="flex items-center space-x-2">
-                    <RadioGroupItem id="option-four" value="option-four" />
+                    <RadioGroupItem
+                        id="option-four"
+                        value="option-four"
+                        @click="onOpt(3)"
+                    />
                     <Label for="option-four">{{ answers[3] }}</Label>
                 </div>
             </RadioGroup>
-            <Button class="mt-4">Submit</Button>
+            <Button class="mt-4" @click="onSubmit()">Submit</Button>
         </div>
     </div>
 </template>
